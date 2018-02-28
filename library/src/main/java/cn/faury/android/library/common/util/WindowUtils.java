54da3e66d249ec8f4246181203cca0d1b45d763f
faury <yucai_fan@163.com>
2018-02-28 10:01:17 +0800

    commit
    v0.0.3

--- library/src/main/java/cn/faury/android/library/common/util/WindowUtils.java
+++ library/src/main/java/cn/faury/android/library/common/util/WindowUtils.java
@@ -2,6 +2,8 @@ package cn.faury.android.library.common.util;
 
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.text.TextPaint;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
@@ -51,6 +53,18 @@ public class WindowUtils {
     }
 
     /**
+     * 根据手机的分辨率从 sp 的单位 转成为 px(像素)
+     *
+     * @param context 上下文对象
+     * @param spValue sp值
+     * @return px值
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
      * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
      *
      * @param context 上下文对象
@@ -60,5 +74,30 @@ public class WindowUtils {
     public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
+    }
+
+
+    /**
+     * 测量字符高度
+     *
+     * @param textPaint 画笔
+     * @param text 字符
+     * @return 字符高度
+     */
+    public static int getTextHeight(TextPaint textPaint, String text) {
+        Rect bounds = new Rect();
+        textPaint.getTextBounds(text, 0, text.length(), bounds);
+        return bounds.height();
+    }
+
+    /**
+     * 测量字符宽度
+     *
+     * @param textPaint 画笔
+     * @param text 字符
+     * @return 字符宽度
+     */
+    public static int getTextWidth(TextPaint textPaint, String text) {
+        return (int) textPaint.measureText(text);
     }
 }
